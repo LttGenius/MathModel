16fb615c08fb744730fbfad1007b5b48764a427d
LttGenius <330470351@qq.com>
2022-01-29 11:28:10 +0800

    pxy/22/1/29

--- arithmetic/LP.py
+++ arithmetic/LP.py
@@ -1,10 +1,10 @@
 #线性规划算法库
 from scipy.optimize import linprog
+import sympy
 import numpy as np
 import copy
 #基础线性规划为
 # linprog(c,Au,Bu,Aeq,Beq,b)
-
 ###########整数规划##############
 
 #分支定界法#####################################################################
@@ -98,22 +98,52 @@ def brandBoundLP(C,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,putValue
 
 """
 def cuttingPlaneApproach(c,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,putValue=False):
+    ###拷贝###############
     if putValue:
         C=[-1*i for i in c]
     else:
         C=copy.deepcopy(c)
-    res=linprog(C,Au,Bu,Aeq,Beq,b)
+    au=copy.deepcopy(Au)
+    bu=copy.deepcopy(Bu)
+    ######################
+    res=linprog(C,au,bu,Aeq,Beq,b)
     newb=copy.deepcopy(b)
     if not res.success:return res#无解
     """
     接下来是割平面求最优解
     """
+    if au:
+        Matrix=np.ones((au.shape[0],au.shape[0]))
+        Matrix=np.hstack((au,Matrix))
+        Matrix=np.hstack((Matrix,Beq))
+        """
+        Matrix:
+            [[a11,a12,a13,...,one1,0,0,...,b1]
+             [a21,a22,a23,...,0,one2,0,...,b2]
+             ...
+             ...
+             [an1,an2,an3,...,0,0,...,onen,bn]
+                                              ]
+        """
+        position=au.shape[1]+1
+    else:#无整数解
+        pass
     while not all(((x-np.floor(x))<limitT or (np.ceil(x)-x)<limitT) for x in res.x):
         """
         分割平面
         """
+        Matrix=np.array(sympy.Matrix(Matrix).rref[0])
+        """
+        Matrix:
+            [[A1,0,0,...,B11,B12,B12,...,Nb1]
+             [0,A2,0,...,B21,B22,B23,...,Nb2]
+             ...
+             ...
+             [0,0,...,An,BN1,BN2,BN3,...,Nbn]
+                                              ]
+        """
         
-        res=linprog(C,Au,Bu,Aeq,Beq,newb)
+        res=linprog(C,au,bu,Aeq,Beq,newb)
         if not res.success:return res#规划失败
     return res
 
@@ -123,11 +153,11 @@ def cuttingPlaneApproach(c,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,
 
 if __name__=='__main__':
     c=[-5,-8]
-    au=[[1,1],[5,9]]
-    bu=[6,45]
+    au=[[1,1],[5,9],[1,0],[0,1]]
+    bu=[6,45,float('inf'),float('inf')]
     x0=[0,float('inf')]
     x1=[0,float('inf')]
     #res=brandBoundLP(c,au,bu,b=(x0,x1),putValue=True)
-    res=linprog(c,au,bu,bounds=(x0,x1))
+    res=linprog(c,au,bu)
     print(res)
 
