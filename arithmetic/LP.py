729177203ed58cb004b85feb14c1744240fe8a2b
LttGenius <330470351@qq.com>
2022-01-30 14:27:27 +0800

    pxy/22/1/30

--- arithmetic/LP.py
+++ arithmetic/LP.py
@@ -105,48 +105,64 @@ def cuttingPlaneApproach(c,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,
         C=copy.deepcopy(c)
     au=copy.deepcopy(Au)
     bu=copy.deepcopy(Bu)
-    ######################
-    res=linprog(C,au,bu,Aeq,Beq,b)
     newb=copy.deepcopy(b)
+    ######################
+    res=linprog(C,au,bu,Aeq,Beq,newb)
     if not res.success:return res#无解
     """
     接下来是割平面求最优解
     """
-    if au:
-        Matrix=np.ones((au.shape[0],au.shape[0]))
-        Matrix=np.hstack((au,Matrix))
-        Matrix=np.hstack((Matrix,Beq))
-        """
-        Matrix:
-            [[a11,a12,a13,...,one1,0,0,...,b1]
-             [a21,a22,a23,...,0,one2,0,...,b2]
-             ...
-             ...
-             [an1,an2,an3,...,0,0,...,onen,bn]
-                                              ]
-        """
-        position=au.shape[1]
-    else:#无整数解
-        pass
+    au=np.array(au)
+    bu=np.array(bu)
     while not all(((x-np.floor(x))<limitT or (np.ceil(x)-x)<limitT) for x in res.x):
         """
         分割平面
         """
-        Matrix=np.array(sympy.Matrix(Matrix).rref[0])
+        if au.any():
+            Matrix=np.eye(au.shape[0])
+            Matrix=np.hstack((au,Matrix))
+            Matrix=np.hstack((Matrix,bu.reshape(au.shape[0],1)))
+            """
+            Matrix:
+                [[a11,a12,a13,...,one1,0,0,...,b1]
+                [a21,a22,a23,...,0,one2,0,...,b2]
+                ...
+                ...
+                [an1,an2,an3,...,0,0,...,onen,bn]
+                                                ]
+            """
+            position0=au.shape[1]
+            position1=au.shape[0]
+            C=np.hstack((C,np.zeros(position1)))
+            newb=np.hstack
+        else:#无整数解
+            return res
+        Matrix=np.array((sympy.Matrix(Matrix).rref())[0])
         """
         Matrix:
-            [[A1,0,0,...,B11,B12,B12,...,Nb1]
-             [0,A2,0,...,B21,B22,B23,...,Nb2]
+            [[1,0,0,...,B11,B12,B12,...,Nb1]
+             [0,1,0,...,B21,B22,B23,...,Nb2]
              ...
              ...
-             [0,0,...,An,BN1,BN2,BN3,...,Nbn]
+             [0,0,...,1,BN1,BN2,BN3,...,Nbn]
                                               ]
         """
         tempAu=[]
         tempBu=[]
         for i in Matrix:
-            for j in i[position:]:
-                tempAu.append(-1*())
+            templist=[]
+            flag=0
+            for j in i[position0:]:
+                if flag == position1:
+                    tempBu.append(-1*(j%1))
+                else:
+                    templist.append(-1*(j%1))
+                flag+=1
+            tempAu.append(templist)
+        a=np.zeros((position1,position1))
+        au=np.hstack((au,a))
+        au=np.vstack((au,np.hstack((np.zeros((position0,position0)),np.array(tempAu)))))
+        bu=np.hstack((bu,np.array(tempBu)))
         res=linprog(C,au,bu,Aeq,Beq,newb)
         if not res.success:return res#规划失败
     return res
@@ -157,11 +173,10 @@ def cuttingPlaneApproach(c,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,
 
 if __name__=='__main__':
     c=[-5,-8]
-    au=[[1,1],[5,9],[1,0],[0,1]]
-    bu=[6,45,float('inf'),float('inf')]
+    au=[[1,1],[5,9]]
+    bu=[6,45]
     x0=[0,float('inf')]
     x1=[0,float('inf')]
-    #res=brandBoundLP(c,au,bu,b=(x0,x1),putValue=True)
-    res=linprog(c,au,bu)
+    res=cuttingPlaneApproach(c,au,bu,b=(x0,x1))
     print(res)
 
