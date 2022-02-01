0c5ce9838f25cb962c0d87ceb3199d902a189cd5
LttGenius <330470351@qq.com>
2022-02-01 18:26:49 +0800

    Merge branch 'main' of github.com:LttGenius/MathModel

--- arithmetic/LP.py
+++ arithmetic/LP.py
@@ -1,13 +1,98 @@
-#线性规划算法库
+"""
+线性规划算法库
+Arithmetic:
+    Basic:
+        松弛问题线性规划 linprog
+        单纯形法 simplexMethod
+    Integer LP:
+        分支定界法 brandBoundLP
+        割平面法
+"""
 from scipy.optimize import linprog
 import sympy
 import numpy as np
 import copy
-#基础线性规划为
+#松弛线性规划
 # linprog(c,Au,Bu,Aeq,Beq,b)
-###########整数规划##############
 
-#分支定界法#####################################################################
+#单纯性法
+"""
+Model:
+    Min(Max) cX
+    S.T.
+        Au<=Bu
+    需要保证X>=0
+    Max和Min由参数putValue确认
+"""
+def simplexMethod(c,Au=None,Bu=None,putValue=False,putForm=False):
+    #构建矩阵
+    """
+    矩阵形状:
+        array[[b0,a10,a11,a12,...,a1n]
+              [b1,a20,a21,a22,...,a2n]
+              ...
+              [bn,an0,an1,an2,...,ann]
+              [Z,C0,C1,C2,C3,c4,..,Cn]]
+    b为由Bu组合成的一列
+    a是由不等式(加上松弛变量变为等式)组合出的矩阵
+    """
+    if not Au:#无约束错误
+        return False
+    if putValue:
+        c=[-1*i for i in c]
+    length=len(Au)
+    temp=np.eye(length)
+    simplexMatrix=np.hstack((np.array(Au),temp))
+    temp=np.array(Bu)
+    temp=temp.reshape(len(temp),1)
+    simplexMatrix=np.hstack((temp,simplexMatrix))
+    temp=np.hstack((np.array([0]),np.hstack((np.array(c),np.array([0 for _ in range(length)])))))
+    simplexMatrix=np.vstack((simplexMatrix,temp))
+    #创立各种系数检验列表
+    """
+    cherkoutMatrix_X:array[Aj,...,An] 基变量，初始挑选为单位矩阵
+    checkoutMatrix_6:array[g0,..,gn] 非基变量检验数
+    """
+    checkoutMatrix_X=np.array([i for i in range(len(c)+1,len(c)+1+length)])
+    position6=simplexMatrix.shape[0]-1
+    checkoutMatrix_6=simplexMatrix[position6][1:]
+    #开始查找最优解:
+    while np.any(checkoutMatrix_6>0):
+        """
+        position:[p0,p1] p1:行 p0:列
+        """
+        position=[]
+        position.append(int(np.argwhere(checkoutMatrix_6==(max(checkoutMatrix_6)))+1))
+        #判断有无解
+        if np.all(checkoutMatrix_6<=0) and np.any(checkoutMatrix_X>=(length+1)):return False
+        #有解执行
+        B=simplexMatrix[0:position6,0]/simplexMatrix[0:position6,position[0]]
+        B[B<=0]=float('inf')
+        if np.all(B==float('inf')):#有无界解
+            return "无界"
+        position.append(int(np.argwhere(B==(min(B)))))#查找到主元素位置
+        #接下里进行初等行变换
+        #将主元素这一行除以主元素
+        B=simplexMatrix[position[1]]=simplexMatrix[position[1]]/simplexMatrix[position[1],position[0]]
+        #其余行减去这一行
+        temp=copy.deepcopy(simplexMatrix[position[1]])
+        for i in range(position6+1):
+            simplexMatrix[i]-=simplexMatrix[i,position[0]]*B
+        simplexMatrix[position[1]]=temp
+        #更新checkoutMatrix_X
+        checkoutMatrix_X[position[1]]=position[0]
+        #更新checkoutMatrix_6
+        checkoutMatrix_6=simplexMatrix[position6][1:]
+    if putValue:
+        simplexMatrix[length,0]=-1*simplexMatrix[length,0]
+    if putForm:
+        return simplexMatrix
+    else:
+        return [simplexMatrix[0:length,0],simplexMatrix[length,0]]
+            
+
+###########整数规划##############
+###########分支定界法############
 """
 Model:
     Min(Max) CX 
@@ -15,7 +100,6 @@ Model:
         AuX<=Bu
         AeqX=Beq
         lb<=X<=ub(b=(lb,ub))
-
 putValue=True:模型是求最大值，会对C进行自动求负
          False:模型求最小值
 limitT:误差阈值
@@ -90,10 +174,8 @@ def brandBoundLP(C,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,putValue
     else:
         res.success='False'
         return res
-#####################################################################################
 
-
-#割平面法#############################################################################
+###########割平面法###############
 """
 
 """
@@ -167,16 +249,14 @@ def cuttingPlaneApproach(c,Au=None,Bu=None,Aeq=None,Beq=None,b=None,limitT=1e-7,
         if not res.success:return res#规划失败
     return res
 
-#######################################################################################
-
-
-
 if __name__=='__main__':
-    c=[-5,-8]
-    au=[[1,1],[5,9]]
-    bu=[6,45]
-    x0=[0,float('inf')]
-    x1=[0,float('inf')]
-    res=cuttingPlaneApproach(c,au,bu,b=(x0,x1))
+    C = [-3,-4] 
+    A = [[2,1],[1,3]]
+    b = [40,30]
+    X0_bounds = [0,float('inf')]
+    X1_bounds = [0,float('inf')]
+    res=simplexMethod(C,A,b,putValue=True,putForm=True)
     print(res)
+    
+
 
